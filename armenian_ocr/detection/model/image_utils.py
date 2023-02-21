73c5beb0bfb1cff466c342f4562a0772ee146ea9
Sipan Muradyan <sipan.muradyan@webbfontaine.com>
2023-02-21 18:02:42 +0400

    remove useless code, update documentation and typing

--- armenian_ocr/detection/model/image_utils.py
+++ armenian_ocr/detection/model/image_utils.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import cv2
 import numpy as np
 
@@ -8,11 +10,10 @@ def load_image(image_path: str) -> np.ndarray:
 
 def normalize_mean_variance(
     input_image: np.ndarray,
-    mean: tuple = (0.485, 0.456, 0.406),
-    variance: tuple = (0.229, 0.224, 0.225),
+    mean: Tuple[float, float, float] = (0.485, 0.456, 0.406),
+    variance: Tuple[float, float, float] = (0.229, 0.224, 0.225),
 ) -> np.ndarray:
-    """
-    Normalize input image for model training
+    """Normalize input image for model training
 
     Args:
         input_image: Input image, should be in RGB
@@ -36,11 +37,10 @@ def normalize_mean_variance(
 
 def denormalize_mean_variance(
     input_image: np.ndarray,
-    mean: tuple = (0.485, 0.456, 0.406),
-    variance: tuple = (0.229, 0.224, 0.225),
+    mean: Tuple[float, float, float] = (0.485, 0.456, 0.406),
+    variance: Tuple[float, float, float] = (0.229, 0.224, 0.225),
 ) -> np.ndarray:
-    """
-    Denormalize input image
+    """Denormalize input image
 
     Args:
         input_image: Input image, should be in RGB
@@ -52,7 +52,7 @@ def denormalize_mean_variance(
     """
     image = input_image.copy()
     image = ((image * variance) + mean) * 255
-    image = np.clip(image, 0, 255).astype(np.uint8)
+    image = np.clip(a=image, a_min=0, a_max=255).astype(np.uint8)
     return image
 
 
@@ -60,11 +60,10 @@ def resize_aspect_ratio(
     image: np.ndarray,
     square_size: int,
     interpolation,
-    rounder_int: int = 32,
     fill_value: int = 0,
     center: bool = True,
-) -> tuple:
-    """Resize img to square size by using interpolation.
+) -> Tuple[np.ndarray, float]:
+    """Resize image to square size by using interpolation.
     Big side of the image will be resized to square size, then small side will be resized
     by the same ratio as big side was.
 
@@ -73,7 +72,6 @@ def resize_aspect_ratio(
         square_size: Size of resized image's big side
         interpolation (cv2 interpolation) : Interpolation type
         in our case always image > square_size
-        rounder_int: Resized image size should be divisible by rounder_int, if not pad some part
         fill_value: Fill value for padding
         center: Pad in a way that original image is in the center
 
@@ -98,10 +96,7 @@ def resize_aspect_ratio(
     # make canvas and paste image
     target_h32, target_w32 = target_h, target_w
     max_side = max(target_h32, target_w32)
-    if target_h % rounder_int != 0:
-        target_h32 = target_h + (rounder_int - target_h % rounder_int)
-    if target_w % rounder_int != 0:
-        target_w32 = target_w + (rounder_int - target_w % rounder_int)
+
     resized = (
         np.ones((max_side, max_side, channel), dtype=np.float32) * fill_value
     )
@@ -126,23 +121,19 @@ def resize_aspect_ratio(
             resized[0:target_h, 0:target_w, 0] = processed
         else:
             resized[0:target_h, 0:target_w, :] = processed
-    target_h, target_w = target_h32, target_w32
 
-    size_heatmap = (target_w // 2, target_h // 2)
-
-    return resized, ratio, size_heatmap
+    return resized, ratio
 
 
 def cvt2heatmap_image(image: np.ndarray) -> np.ndarray:
-    """
-    Make heatmap from model predicted maps
+    """Make heatmap from model predicted maps
 
     Args:
         image: Vertical stack of region and affinity boxes
 
     Returns:
-        Heatmap of the input
+        Input heatmap
     """
-    image = (np.clip(image, 0, 1) * 255).astype(np.uint8)
-    image = cv2.applyColorMap(image, cv2.COLORMAP_JET)
+    image = (np.clip(a=image, a_min=0, a_max=1) * 255).astype(np.uint8)
+    image = cv2.applyColorMap(src=image, colormap=cv2.COLORMAP_JET)
     return image
