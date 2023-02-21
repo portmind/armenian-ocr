0a37e50f9d3468f5401af6f4f1314c5c2cf4dbad
Sipan Muradyan <sipan.muradyan@webbfontaine.com>
2023-02-21 18:06:20 +0400

    add short flag names, add new args, check cuda availability

--- ocr.py
+++ ocr.py
@@ -2,6 +2,7 @@ import json
 from argparse import ArgumentParser
 
 import numpy as np
+import torch.cuda
 from PIL import Image
 
 from armenian_ocr import OcrWrapper
@@ -9,31 +10,47 @@ from armenian_ocr import OcrWrapper
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument(
+        "-dd",
         "--detection_dir",
         type=str,
         help="Path to the detection model directory.",
     )
     parser.add_argument(
+        "-rd",
         "--recognition_dir",
         type=str,
         help="Path to the recognition model directory.",
     )
-    parser.add_argument("--image_path", type=str, help="Path to the image.")
     parser.add_argument(
-        "--output_path", type=str, help="Path to the output file."
+        "-i", "--image_path", type=str, help="Path to the image."
     )
-    parser.add_argument("--cuda", action="store_true", help="Use cuda.")
+    parser.add_argument(
+        "-o", "--output_path", type=str, help="Path to the output file."
+    )
+    parser.add_argument(
+        "-l", "--layout", action="store_true", help="Detect layout."
+    )
+    parser.add_argument(
+        "-t", "--timer", action="store_true", help="Show processing times."
+    )
+    parser.add_argument(
+        "-cuda", "--cuda", action="store_true", help="Use cuda."
+    )
     args = parser.parse_args()
+
+    device = "cuda" if args.cuda and torch.cuda.is_available() else "cpu"
 
     ocr = OcrWrapper()
     ocr.load(
-        args.detection_dir,
-        args.recognition_dir,
-        device="cuda" if args.cuda else "cpu",
+        det_model_dir=args.detection_dir,
+        rec_model_dir=args.recognition_dir,
+        device=device,
     )
 
-    img = np.array(Image.open(args.image_path))
-    predictions = ocr.predict(img)
+    image = np.array(Image.open(args.image_path))
+    predictions = ocr.predict(
+        image=image, predict_layout=args.layout, timer=args.timer
+    )
 
-    with open(args.output_path, "w", encoding="utf-8") as f:
-        json.dump(predictions, f, ensure_ascii=False)
+    with open(args.output_path, "w", encoding="utf-8") as fp:
+        json.dump(obj=predictions, fp=fp, ensure_ascii=False)
