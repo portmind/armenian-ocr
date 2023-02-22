5336b45de6881cbb7a84285165685cc39f48b6db
Sipan Muradyan <sipan.muradyan@webbfontaine.com>
2023-02-22 15:14:33 +0400

    add pdf support, add paging to output

--- ocr.py
+++ ocr.py
@@ -1,8 +1,10 @@
 import json
+import time
 from argparse import ArgumentParser
 
 import numpy as np
 import torch.cuda
+from pdf2image import convert_from_path
 from PIL import Image
 
 from armenian_ocr import OcrWrapper
@@ -22,7 +24,10 @@ if __name__ == "__main__":
         help="Path to the recognition model directory.",
     )
     parser.add_argument(
-        "-i", "--image_path", type=str, help="Path to the image."
+        "-d",
+        "--document_path",
+        type=str,
+        help="Path to the document (image or PDF).",
     )
     parser.add_argument(
         "-o", "--output_path", type=str, help="Path to the output file."
@@ -47,22 +52,42 @@ if __name__ == "__main__":
         device=device,
     )
 
-    pil_image = Image.open(args.image_path)
-    np_image = np.array(pil_image)
+    start = time.time()
 
-    if np_image.shape[-1] == 4:  # handle images with alpha channel
-        pil_image.load()
-        blended_image = Image.new(
-            mode="RGB", size=pil_image.size, color=(255, 255, 255)
+    document_extension = args.document_path.split(".")[-1].lower()
+
+    if document_extension == "pdf":
+        pil_images = convert_from_path(args.document_path)
+        np_images = [np.array(pil_image) for pil_image in pil_images]
+    elif document_extension in ["png", "jpg", "jpeg"]:
+        pil_images = convert_from_path(args.document_path)
+        np_images = [np.array(pil_image) for pil_image in pil_images]
+        pil_image = Image.open(args.document_path)
+        np_image = np.array(pil_image)
+
+        if np_image.shape[-1] == 4:  # handle images with alpha channel
+            pil_image.load()
+            blended_image = Image.new(
+                mode="RGB", size=pil_image.size, color=(255, 255, 255)
+            )
+            blended_image.paste(
+                im=pil_image, mask=pil_image.split()[3]
+            )  # blend alpha channel
+            np_image = np.array(blended_image)
+        np_images = [np_image]
+    else:
+        raise NotImplementedError(
+            f"Documents with {document_extension} extension are not supported"
         )
-        blended_image.paste(
-            im=pil_image, mask=pil_image.split()[3]
-        )  # blend alpha channel
-        np_image = np.array(blended_image)
 
-    predictions = ocr.predict(
-        image=np_image, predict_layout=args.layout, timer=args.timer
-    )
+    predictions = [
+        ocr.predict(
+            image=np_image, predict_layout=args.layout, timer=args.timer
+        )
+        for np_image in np_images
+    ]
+
+    print(f"Whole process took {time.time() - start:.2f} seconds")
 
     with open(args.output_path, "w", encoding="utf-8") as fp:
         json.dump(obj=predictions, fp=fp, ensure_ascii=False)
